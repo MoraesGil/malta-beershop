acdbfd2a442013782ee81353e820b4032ab16483
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-03 11:29:39 -0300

    Iniciado novo Layout Produtos

--- Site/pg/marcas.php
+++ Site/pg/marcas.php
@@ -1,32 +1,27 @@
-<section class="subpage-banner contact-banner" style="margin-bottom: 0px !important">
-	<div class="container">
-		<div class="row header-group">
-			<div class="col-sm-8 col-sm-12">
-				<h1>MARCAS</h1>
-				<p>NOSSAS MARCAS</p>
-			</div>
-			<div class="col-xs-4 hidden-xs">
-				<ol class="breadcrumb navegacao" >
-					<li>Você está em: </li>
-					<li><a href="home">Home</a></li>
-					<li class="active">Marcas</li>
-				</ol>
+
+	<section class="subpage-banner blog-classic-banner">
+		<div class="container">
+			<div class="row header-group">
+				<div class="col-sm-8 col-sm-12">
+					<h1>MARCAS</h1>
+					<p>MALTA CERVEJARIA</p>
+				</div>
+				<div class="col-xs-4 hidden-xs">
+					<ol class="breadcrumb navegacao">
+						<li>Você está em: </li>
+						<li><a href="home">Home</a></li>
+						<li class="active">Marcas</li>
+					</ol>
+				</div>
 			</div>
 		</div>
+	</section>
+
+
+<div class="container-fluid" style="background:url(_/images/marcas/1/background.jpg)  no-repeat; margin 0; width:100%">
+	<div class="row">
+		<div class="col-md-6">
+				<img class="img-responsive" src="_/images/marcas/1/beer.png" alt="" />
+		</div>
 	</div>
-</section>
-
-<style>
-h2{
-	font-weight:bold;text-shadow:
-	1px 1px 2px #000000;
-}
-.slide-description-text	{
-	font-weight:bold;
-	text-shadow: 1px 1px 2px #000000;
-}
-</style>
-
-<section style="magin: 0 0 0 0" >
-	<img class="img-responsive" src="_/images/marcas.jpg" alt="" />
-</section><!-- Sub Slider -->
+</div>	<!-- Bottom Section -->
