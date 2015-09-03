0733a200bd737862ebc2f4722f6197ba31365abe
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-03 17:41:48 -0300

    redesign responsivo, novas imagens menor kb maior qualidade

--- Site/pg/marcas.php
+++ Site/pg/marcas.php
@@ -1,27 +1,120 @@
 
-	<section class="subpage-banner blog-classic-banner">
-		<div class="container">
-			<div class="row header-group">
-				<div class="col-sm-8 col-sm-12">
-					<h1>MARCAS</h1>
-					<p>MALTA CERVEJARIA</p>
-				</div>
-				<div class="col-xs-4 hidden-xs">
-					<ol class="breadcrumb navegacao">
-						<li>Você está em: </li>
-						<li><a href="home">Home</a></li>
-						<li class="active">Marcas</li>
-					</ol>
+<section class="subpage-banner blog-classic-banner">
+	<div class="container">
+		<div class="row header-group">
+			<div class="col-sm-8 col-sm-12">
+				<h1>MARCAS</h1>
+				<p>MALTA CERVEJARIA</p>
+			</div>
+			<div class="col-xs-4 hidden-xs">
+				<ol class="breadcrumb navegacao">
+					<li>Você está em: </li>
+					<li><a href="home">Home</a></li>
+					<li class="active">Marcas</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</section>
+<style media="screen">
+.see-more {
+	/* color: #000000; */
+	text-transform: uppercase;
+	font-weight: 300;
+	padding: 15px 30px;
+	-webkit-box-shadow: 0 4px 0 #bdc3c7;
+	box-shadow: 0 4px 0 #bdc3c7;
+	-webkit-border-radius: 3px;
+	border-radius: 3px;
+	background-clip: padding-box;
+	background-color: #e3e9eb;
+	position: relative;
+	top: 0;
+}
+.see-more:hover{
+	background-color: #ffd32c;
+	text-decoration: none;
+	color: #ffffff;
+	-webkit-box-shadow: 0 4px 0 #e2bf3a;
+	box-shadow: 0 4px 0 #e2bf3a;
+}
+
+h2{
+	margin-top: 0;
+	font-size: 33px;
+	color: #ffffff;
+}
+p{
+	color: white;
+}
+
+</style>
+
+<div class="" style="background:url(_/images/marcas/1/background.jpg); margin 0; width:100%">
+	<div class="row">
+		<div class="col-xs-12 col-sm-4 col-sm-offset-3 col-md-3 col-md-offset-3 col-xs-12 col-lg-3 col-lg-offset-3">
+			<img class=" center-block" src="_/images/marcas/1/beer.png" alt="" />
+
+		</div>
+		<div class="col-xs-12 col-sm-4 col-md-3  col-xs-12 beer-description text-center" >
+			<p>
+				<h2>CERVEJA MALTA PILSEN</h2>
+			</p>
+			<p>
+				Embalagens:<br>
+				Garrafa retornável 600 ml<br>
+				Lata 473 ml<br>
+				Lata 350 ml<br>
+				Lata 269 ml<br>
+
+			</p>
+			<p>
+				<a href="malta-pilsen" class="see-more">Veja Mais</a>
+			</p>
+		</div>
+	</div>
+	<div class="" style="background-image:url(_/images/marcas/1/mesa.jpg); repeat-y; margin 0; width:100%">
+
+	</div>
+</div>	<!-- Bottom Section -->
+
+<section class="sub-slider bg-pilsen" data-type="background" data-speed="3" style="margin-bottom: 0px !important">
+	<div class="container">
+		<div class="row">
+			<div class="col col-sm-12">
+				<div id="sequence-sub-slider">
+
+					<ul class="sequence-canvas">
+						<li class="animate-in">
+							<div class="row">
+								<div class="col col-sm-5 col-sm-offset-1 col-xs-12 slide-image-container">
+									<img class="img-responsive absolute-pos slide-image" src="_/images/marcas/item-pilsen.png">
+								</div>
+								<div class="col col-sm-5 col-xs-12">
+									<div class="slide-panel absolute-pos">
+										<section style="margin-top: 90px">
+											<h2>CERVEJA MALTA PILSEN</h2>
+										</section>
+
+										<hr>
+										<p class="slide-description-text">
+											Embalagens:<br>
+											Garrafa retornável 600 ml<br>
+											Lata 473 ml<br>
+											Lata 350 ml<br>
+											Lata 269 ml<br>
+										</p>
+										<div class="slide-readmore">
+											<a href="malta-pilsen" class="button-more">Veja Mais</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li><!-- Frame 1 -->
+					</ul>
 				</div>
 			</div>
 		</div>
-	</section>
-
-
-<div class="container-fluid" style="background:url(_/images/marcas/1/background.jpg)  no-repeat; margin 0; width:100%">
-	<div class="row">
-		<div class="col-md-6">
-				<img class="img-responsive" src="_/images/marcas/1/beer.png" alt="" />
-		</div>
 	</div>
-</div>	<!-- Bottom Section -->
+	<div style="background-image:url('_/images/marcas/bgs/bg-footer-malta-pilsen.jpg'); height:50px; width:100%;">    </div>
+</section><!-- Sub Slider -->
