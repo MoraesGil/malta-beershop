f47eeaebf285ab8f90a2c39d6d92a99d74f36826
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-20 19:22:47 -0300

    Pagina Concluida

--- Site/pg/pilsen.php
+++ Site/pg/pilsen.php
@@ -1,25 +1,127 @@
-<?php
-if(isset($_REQUEST['marca'])) {
-  $marca = $_REQUEST['marca'];
+<style>
+.bg{
+
+  padding-top:15%;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/Pilsen/bg.jpg) #652600 no-repeat;
+  width:100%
+  height: 100%;
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-width: 90%;
+}
+
+.CERVEJA_PILSEN {
+  font-size: 5em;
+  font-family: "Helvetica";
+  color: rgb(207, 19, 24);
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: center;
+  text-shadow: 1px 1px 2px #000000;
+  padding-bottom: 10px;
+
 }
 
 
-?>
+</style>
 
-<div style="background:url(_/images/pilsen-bc.jpg) #652600 no-repeat; width:100%">
+<div class="blog_classic bg">
   <div class="container blog_classic_posts">
     <div class="row">
       <div class="col-md-6">
-        <div class="row" style="padding-top:25%; padding-left:3%; padding-right: 3%;">
-          <img src="_/images/malta-pilsen-title.png" class="img-responsive"  />
-          <img src="_/images/malta-pilsen-itens.png" class="img-responsive"  />
-          <img src="_/images/pilsen-quadro.png" class="img-responsive" />
+        <div class="row">
+          <div class="col-md-6 col-lg-6">
+            <div class="CERVEJA_PILSEN">
+              <em>
+                <p>
+                  CERVEJA
+                </p>
+                <p>
+                  PILSEN
+                </p>
+              </em>
+
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/Pilsen/1.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                600 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/Pilsen/2.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                473 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/Pilsen/3.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                350 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/Pilsen/4.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                269 ml
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row hidden-xs hidden-sm ">
+          <img class="img-responsive center-block seta" src="_/images/marcas-detalhado/Pilsen/seta.png" alt="" />
+        </div>
+        <div class="row">
+          <img class="img-responsive center-block" src="_/images/marcas-detalhado/Pilsen/info.png" alt="" />
         </div>
       </div>
-      <div class="col-md-6">
-        <img src="_/images/pilsen.png" class="img-responsive"  width="100%" style="margin-top:25%"/>
-      </div>
 
+      <div class="col-md-6 hidden-xs hidden-sm col-lg-6">
+        <img id="principal" class="img-responsive center-block" src="_/images/marcas-detalhado/Pilsen/principal.png" alt="" />
+      </div>
     </div>
   </div>
 </div>	<!-- Bottom Section -->
