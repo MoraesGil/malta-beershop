76af8157f448f5542ed0089c597372889501f579
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-21 15:20:04 -0300

    Paginal finalizada

---
+++ Site/pg/pilsen-sem-alcool.php
@@ -0,0 +1,116 @@
+<style>
+.bg{
+
+  padding-top:15%;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/Pilsen/bg.jpg) #652600 no-repeat;
+  width:100%
+  height: 100%;
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-width: 90%;
+}
+
+.titulo {
+  font-size: 5em;
+  font-family: "Helvetica";
+  color: rgb(0, 81, 131);
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: center;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+
+
+
+
+@media (max-width: 480px) {
+  .titulo {
+    padding-top: 15%;
+    font-size: 3em;
+  }
+}
+
+
+
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-6 col-lg-6">
+            <div class="titulo">
+              <em>
+                <p>
+                  CERVEJA PILSEN
+                </p>
+                <p>
+                  SEM ÁLCOOL
+                </p>
+              </em>
+
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/pilsen-sem-alcool/1.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                600 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/pilsen-sem-alcool/2.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                473 ml
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+
+          <div class="row hidden-xs hidden-sm ">
+            <img class="img-responsive center-block seta" src="_/images/marcas-detalhado/pilsen-sem-alcool/seta.png" alt="" />
+          </div>
+          <div class="row">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/pilsen-sem-alcool/info.png" alt="" />
+          </div>
+        </div>
+
+        <div class="col-md-6 hidden-xs hidden-sm col-lg-6">
+          <img id="principal" class="img-responsive center-block" src="_/images/marcas-detalhado/pilsen-sem-alcool/principal.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>	<!-- Bottom Section -->
