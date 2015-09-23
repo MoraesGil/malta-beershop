b98c70f9a37d6b9fc94f988f94124c20b29ade38
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-23 15:26:15 -0300

    pagina golden beer finalizada

---
+++ Site/pg/golden-beer.php
@@ -0,0 +1,164 @@
+<style>
+.bg{
+  padding-top: 80px;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/golden-beer/bg.jpg) #688082 no-repeat;
+  width:100%;
+  background-size: cover;
+
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-height: 500px  ;
+}
+
+#info{
+  font-size: 20px;
+  font-family: "Helvetica";
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.226;
+  text-align: left;
+  margin-top: 20px;
+  text-shadow: 2px 2px 3px #000000;
+}
+
+#info em{
+  font-weight: 200;
+}
+
+
+.titulo {
+  font-size: 5em;
+  font-family: "Helvetica";
+  color: #CF1318;
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: left;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+
+
+@media (max-width: 995px) {
+  .titulo {
+    text-align: center;
+  }
+}
+@media (max-width: 480px) {
+  .titulo {
+    font-size: 3em;
+    text-align: center;
+  }
+}
+
+
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+      <div class="col-md-8">
+        <!-- titulo -->
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="titulo">
+              <em>
+                <p>
+                  CERVEJA
+                </p>
+                <p>
+                  GOLDEN BEER
+                </p>
+              </em>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- miniaturas -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/golden-beer/1.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                600 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/golden-beer/2.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                350 ml
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row hidden-xs hidden-sm">
+          <div class="col-md-12">
+            <img class="img-responsive pull-right seta" src="_/images/marcas-detalhado/golden-beer/seta.png" alt="" />
+          </div>
+        </div>
+        <!-- info -->
+        <div class="row ">
+          <div class="col-md-12">
+            <div class="row">
+              <div class="col-md-4">
+                <img class="img-responsive pull-right seta hidden-xs hidden-sm" src="_/images/marcas-detalhado/golden-beer/principal2.png" alt="" />
+              </div>
+              <div class="col-md-8" id="info">
+                <p>
+                  Informação Nutricional: <br>
+                  <em>Definição/Tipo: Cerveja Tipo Pilsen<br>
+                    Graduação Alcoólica: 4,8%</em>
+
+                  </p>
+                  <p>
+                    Ingredientes: <br>
+                    <em>Água, Malte, Carboidratos, Cereais não malteados e Lúpulo</em>
+                  </p>
+                  <p>
+                    Embalagens: <br>
+                    <em>
+                      Garrafa retornável 600 ml <br>
+                      Lata 350 ml
+                    </em>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 hidden-xs hidden-sm">
+          <img class="img-responsive pull-right" src="_/images/marcas-detalhado/golden-beer/principal.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>	<!-- Bottom Section -->
