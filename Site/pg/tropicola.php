9b8df0a519e3277063b2f92ba964d4eaf9a49fad
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-24 17:59:05 -0300

    pagina tropicola finalizada

---
+++ Site/pg/tropicola.php
@@ -0,0 +1,206 @@
+<style>
+.bg{
+  padding-top: 80px;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/tropicola/bg.jpg) #688082 no-repeat;
+  width:100%;
+  background-size: cover;
+
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+  font-weight: bold;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-height: 500px  ;
+}
+
+#info{
+  font-size: 20px;
+  font-family: "Helvetica";
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.226;
+  text-align: left;
+  margin-top: 20px;
+  text-shadow: 2px 2px 3px #000000;
+}
+
+#info em{
+  font-weight: 200;
+}
+
+
+.titulo {
+  font-size: 55px;
+  font-family: "Helvetica";
+  color: #ccc;
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: left;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+
+
+@media (max-width: 995px) {
+  .titulo {
+    text-align: center;
+  }
+}
+@media (max-width: 480px) {
+  .titulo {
+    font-size: 3em;
+    text-align: center;
+  }
+}
+
+.btn-zero {
+  color: #CCC;
+  text-transform: uppercase;
+  font-weight: 10;
+  padding: 5px 30px;
+  box-shadow: 0 4px 0 #41464A;
+  border-radius: 3px;
+  background-clip: padding-box;
+  background-color: #000000;
+  top: 0;
+  font-weight: bold;
+  font-style: italic;
+}
+
+a:hover, a:focus {
+    color: #FE0005;
+    text-decoration: none;
+}
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- titulo -->
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="pull-right">
+              <a id="btn-change" class="btn-zero" href="tropicola-zero">Tropicola Zero</a>
+            </div>
+            <div class="titulo">
+              <em>
+                <p>
+                  TROPICOLA
+                </p>
+              </em>
+            </div>
+          </div>
+        </div>
+        <!-- miniaturas -->
+        <div class="row">
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/tropicola/1.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                2 Litros
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/tropicola/2.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                605 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/tropicola/3.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                600 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/tropicola/4.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                350 ml
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row ">
+          <div class="col-md-6">
+
+            <div class="row">
+              <img class="img-responsive pull-right seta hidden-xs hidden-sm" src="_/images/marcas-detalhado/tropicola/seta.png" alt="" />
+            </div>
+
+            <div class="row">
+              <div class="col-xs-12 col-sm-12">
+                <div id="info">
+                  <p>
+                    Informação Nutricional: <br>
+                    <em>Teor Calórico: 80 kcal/200 ml</em>
+
+                  </p>
+                  <p>
+                    Ingredientes: <br>
+                    <em>Água gaseificada, Açúcar, Extrato de noz de cola</em>
+                  </p>
+                  <p>
+                    Embalagens: <br>
+                    <em>
+                      Garrafa 2 litros <br>
+                      Garrafa retornável 605 ml <br>
+                      Garrafa 600 ml <br>
+                      Lata 350 ml
+                    </em>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 hidden-xs hidden-sm ">
+            <img class="img-responsive center-block seta" src="_/images/marcas-detalhado/tropicola/principal.png" alt="" />
+
+          </div>
+        </div>
+
+      </div>
+
+
+    </div>
+  </div>
+</div>	<!-- Bottom Section -->
