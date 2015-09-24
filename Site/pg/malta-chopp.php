ad44c4485784c3483c7242600f1415f62b19f8b6
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-24 10:00:54 -0300

    Finalizada Pagina CHOPP

---
+++ Site/pg/malta-chopp.php
@@ -0,0 +1,166 @@
+<style>
+.bg{
+  padding-top: 80px;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/chopp/bg.jpg) #688082 no-repeat;
+  width:100%;
+  background-size: cover;
+
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+  font-weight: bold;
+}
+
+
+#principal{
+  max-height: 500px  ;
+}
+
+#info{
+  font-size: 20px;
+  font-family: "Helvetica";
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.226;
+  text-align: left;
+  margin-top: 20px;
+  text-shadow: 2px 2px 3px #000000;
+}
+
+#info em{
+  font-weight: 200;
+}
+
+.img-circle{
+  text-shadow: 2px 2px 3px #000000;
+}
+
+.titulo {
+  font-size: 55px;
+  font-family: "Helvetica";
+  color: #fff;
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: left;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+
+
+@media (max-width: 995px) {
+  .titulo {
+    text-align: center;
+  }
+}
+@media (max-width: 480px) {
+  .titulo {
+    font-size: 3em;
+    text-align: center;
+  }
+}
+
+
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- titulo -->
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="titulo">
+              <em>
+                <p>
+                  MALTA CHOPP
+                </p>
+
+              </em>
+
+            </div>
+          </div>
+        </div>
+        <!-- miniaturas -->
+        <div class="row hidden-md hidden-lg">
+          <div class="col-xs-12 col-sm-4">
+            <img class="img-responsive img-circle center-block" src="_/images/marcas-detalhado/chopp/1.png" alt="" />
+            <div class="infomini">
+              <p>
+                CHOPP
+              </p>
+
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4">
+            <img class="img-responsive img-circle center-block" src="_/images/marcas-detalhado/chopp/2.png" alt="" />
+            <div class="infomini">
+              <p>
+                CHOPP ESCURO
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <img class="img-responsive img-circle center-block" src="_/images/marcas-detalhado/chopp/3.png" alt="" />
+            <div class="infomini">
+              <p>
+                CHOPP GOLDEN
+              </p>
+            </div>
+          </div>
+
+
+        </div>
+
+
+        <div class="row ">
+          <div class="col-md-6">
+            <div class="row">
+              <div class="col-sm-12">
+                <img class="img-responsive center-block hidden-xs hidden-sm" style="margin-bottom:2px" src="_/images/marcas-detalhado/CHOPP/principal.png" alt="" />
+
+                <img class="img-responsive center-block hidden-xs hidden-md hidden-lg" style="margin-bottom:2px" src="_/images/marcas-detalhado/CHOPP/info2.png" alt="" />
+
+                <div id="info" class="hidden-sm hidden-md hidden-lg">
+                  <p>
+                    Informação Nutricional: <br>
+                    <em>Definição/Tipo: Chopp claro tipo Pilsen <br>
+                      Graduação Alcoólica: 4,8 %</em>
+                    </p>
+
+                    <p>
+                      Ingredientes: <br>
+                      <em>Água, Malte, Carboidratos, Cereais não malteados e Lúpulo</em>
+                    </p>
+                    <p>
+                      Embalagens: <br>
+                      <em>
+                        Barris Tipo Keg de 5L, 10L, 30L e 50L
+                      </em>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 hidden-xs hidden-sm ">
+              <img class="img-responsive center-block seta" src="_/images/marcas-detalhado/CHOPP/info.png" alt="" />
+
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+    </div>
+  </div>	<!-- Bottom Section -->
