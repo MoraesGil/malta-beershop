dfa312e842a438072075987ca58c607921e41add
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-24 16:49:19 -0300

    Pagina finalizada Club Soda

---
+++ Site/pg/club-soda-cristalina.php
@@ -0,0 +1,131 @@
+<style>
+.bg{
+  padding-top: 80px;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/club-soda-cristalina/bg.jpg) #688082 no-repeat;
+  width:100%;
+  background-size: cover;
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+  font-weight: bold;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-width: 90%;
+}
+
+.titulo {
+  font-size: 55px;
+  font-family: "Helvetica";
+  color: #0A774A;
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: left;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+@media (max-width: 991px) {
+  .titulo {
+    font-size: 4em;
+    text-align: center;
+  }
+}
+
+@media (max-width: 991px) {
+  .titulo {
+    font-size: 2em;
+    text-align: center;
+  }
+}
+#info{
+  font-size: 20px;
+  font-family: "Helvetica";
+  color: #0A774A;
+  font-weight: bold;
+  line-height: 1.226;
+  text-align: left;
+  margin-top: 20px;
+
+}
+
+#info em{
+  font-weight: 200;
+}
+
+@media (max-width: 768px) {
+  #info {
+    text-align: center;
+  }
+}
+
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+
+      <div class="col-md-5">
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="titulo">
+              <em>
+                <p>
+                  CLUB SODA
+                </p>
+                <p>
+                  CRISTALINA
+                </p>
+              </em>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <img class="img-responsive center-block hidden-md hidden-lg" src="_/images/marcas-detalhado/club-soda-cristalina/mini.png" alt="" />
+            <img class="img-responsive center-block hidden-xs hidden-sm seta" src="_/images/marcas-detalhado/club-soda-cristalina/seta.png" alt="" />
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-12 col-lg-12">
+            <div id="info">
+              <p>
+                Informação Nutricional: <br>
+                <em>Teor Calórico: 0 kcal/100 ml</em>
+
+              </p>
+              <p>
+                Ingredientes: <br>
+                <em>Agua natural gaseificada.</em>
+              </p>
+              <p>
+                Embalagens: <br>
+                <em>             Garrafa retornável 600 ml         </em>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-7 hidden-xs hidden-sm">
+        <img id="principal" class="img- center-block" src="_/images/marcas-detalhado/club-soda-cristalina/principal.png" alt="" />
+      </div>
+    </div>
+  </div>
+</div>	<!-- Bottom Section -->
