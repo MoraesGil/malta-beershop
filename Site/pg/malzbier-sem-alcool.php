3c0252140ae2fe1454e86ac6d4bcbcdbd1835896
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-25 14:29:05 -0300

    Finalizada Malzbier sem alcool

---
+++ Site/pg/malzbier-sem-alcool.php
@@ -0,0 +1,147 @@
+<style>
+.bg{
+  padding-top: 80px;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/malzbier-sem-alcool/bg.jpg) #688082 no-repeat;
+  width:100%;
+  background-size: cover;
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+  font-weight: bold;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-width: 90%;
+}
+
+#info{
+  font-size: 20px;
+  font-family: "Helvetica";
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.226;
+  text-align: left;
+  margin-top: 20px;
+  text-shadow: 2px 2px 3px #000000;
+}
+
+#info em{
+  font-weight: 200;
+}
+
+.titulo {
+  font-size: 55px;
+  font-family: "Helvetica";
+  color: #FFF;
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: left;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+@media (max-width: 991px) {
+  .titulo {
+    font-size: 3em;
+    text-align: center;
+  }
+}
+
+
+
+
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-12 col-lg-12">
+            <div class="titulo">
+              <em>
+                <p>
+                CERVEJA MALZBIER
+                </p>
+                <p>
+                  SEM ÁLCOOL
+                </p>
+              </em>
+
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/malzbier-sem-alcool/1.png" alt="" />
+            <div class="infomini">
+              <p>
+                Garrafa
+              </p>
+              <p>
+                600 ml
+              </p>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+            <img class="img-responsive center-block" src="_/images/marcas-detalhado/malzbier-sem-alcool/2.png" alt="" />
+            <div class="infomini">
+              <p>
+                Lata
+              </p>
+              <p>
+                473 ml
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+
+          <div class="row hidden-xs hidden-sm ">
+            <img class="img-responsive center-block seta" src="_/images/marcas-detalhado/malzbier-sem-alcool/seta.png" alt="" />
+          </div>
+          <div class="row">
+            <div id="info">
+              <p>
+                Informação Nutricional: <br>
+                <em>Definição/Tipo: Cerveja Tipo Malzbier <br>
+                  Graduação Alcoólica: 0 %</em>
+
+                </p>
+                <p>
+                  Ingredientes: <br>
+                  <em>Água, Malte, Carboidratos, Cereais não malteados e Lúpulo</em>
+                </p>
+                <p>
+                  Embalagens: <br>
+                  <em>
+                    Garrafa retornável 600 ml <br>
+                    Lata 473 ml <br>
+                    Lata 350 ml
+                  </em>
+                </p>
+              </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 hidden-xs hidden-sm col-lg-6">
+          <img id="principal" class="img-responsive center-block" src="_/images/marcas-detalhado/malzbier-sem-alcool/principal.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>	<!-- Bottom Section -->
