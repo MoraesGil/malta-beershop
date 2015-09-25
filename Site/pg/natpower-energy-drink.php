cd7529a2d7898ef0f3c3788f9ef655f13c734c10
Gilberto Moraes <mr.moraes.gil@gmail.com>
2015-09-25 15:34:34 -0300

    pagina concluida

---
+++ Site/pg/natpower-energy-drink.php
@@ -0,0 +1,203 @@
+<style>
+.bg{
+  padding-top: 80px;
+  padding-left:3%;
+  padding-right: 3%;
+  background:url(_/images/marcas-detalhado/NatPower/bg.jpg) #688082 no-repeat;
+  width:100%;
+  background-size: cover;
+
+}
+
+.infomini{
+  font-size: 25px;
+  font-family: "Helvetica";
+  color: rgb(252, 248, 247);
+  line-height: 1.226;
+  text-align: center;
+  padding-top: 5%;
+  font-weight: bold;
+  text-shadow: 2px 2px 3px #000000;
+}
+.seta{
+  margin-top: 20px;
+  margin-bottom: 20px;
+  max-width: 80%;
+}
+
+#principal{
+  max-height: 500px  ;
+}
+
+#info{
+  font-size: 20px;
+  font-family: "Helvetica";
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.226;
+  text-align: left;
+  margin-top: 20px;
+  text-shadow: 2px 2px 3px #000000;
+}
+
+#info em{
+  font-weight: 200;
+}
+
+
+.titulo {
+  font-size: 55px;
+  font-family: "Helvetica";
+  color: #FFC70E;
+  font-weight: bold;
+  line-height: 0.979;
+  text-align: left;
+  text-shadow: 2px 2px 3px #000000;
+  padding: 10px 10px 10px 10px;
+
+}
+
+
+@media (max-width: 995px) {
+  .titulo {
+    text-align: center;
+  }
+}
+@media (max-width: 480px) {
+  .titulo {
+    font-size: 3em;
+    text-align: center;
+  }
+}
+
+.btn-zero {
+  color: #FE0005;
+  text-transform: uppercase;
+  font-weight: 10;
+  padding: 5px 30px;
+  box-shadow: 0 4px 0 #41464A;
+  border-radius: 3px;
+  background-clip: padding-box;
+  background-color: #000000;
+  top: 0;
+  font-weight: bold;
+  font-style: italic;
+  margin-bottom: 20px;
+  margin-top:10px;
+
+}
+
+a:hover, a:focus {
+  color: #CCC;
+  text-decoration: none;
+}
+</style>
+
+<div class="blog_classic bg">
+  <div class="container blog_classic_posts">
+    <div class="row">
+      <div class="col-md-12">
+        <!-- titulo -->
+        <div class="row">
+          <div class="col-md-12">
+
+            <div class="titulo">
+            <em>
+              <p>
+                TROPICOLA
+              </p>
+            </em>
+          </div>
+
+        </div>
+      </div>
+      <!-- miniaturas -->
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <img class="img-responsive center-block" src="_/images/marcas-detalhado/NatPower/1.png" alt="" />
+          <div class="infomini">
+            <p>
+              Garrafa
+            </p>
+            <p>
+              2 Litros
+            </p>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <img class="img-responsive center-block" src="_/images/marcas-detalhado/NatPower/2.png" alt="" />
+          <div class="infomini">
+            <p>
+              Garrafa
+            </p>
+            <p>
+              1 Litro
+            </p>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <img class="img-responsive center-block" src="_/images/marcas-detalhado/NatPower/3.png" alt="" />
+          <div class="infomini">
+            <p>
+              Garrafa
+            </p>
+            <p>
+              600 ml
+            </p>
+          </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+          <img class="img-responsive center-block" src="_/images/marcas-detalhado/NatPower/4.png" alt="" />
+          <div class="infomini">
+            <p>
+              Lata
+            </p>
+            <p>
+              269 ml
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="row ">
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12">
+              <div id="info">
+                <p>
+                  Informação Nutricional: <br>
+                  <em>Teor Calórico – 78 kcal / 200 ml</em>
+
+                </p>
+                <p>
+                  Ingredientes: <br>
+                  <em>Água gaseificada, açúcar, taurina, cafeína e glucoronolactona.</em>
+                </p>
+                <p>
+                  Embalagens: <br>
+                  <em>
+                    Garrafa 2 litros <br>
+                    Garrafa 1 litro <br>
+                    Garrafa 600 ml <br>
+                    Lata 269 ml
+                  </em>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 hidden-xs hidden-sm ">
+          <img class="img-responsive center-block seta" src="_/images/marcas-detalhado/NatPower/principal.png" alt="" />
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+</div>
+</div>	<!-- Bottom Section -->
